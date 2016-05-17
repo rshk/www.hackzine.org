de8b1ae1e25930a935c5bf8fb9ff5a6969ab0e29
Samuele Santi <redshadow@hackzine.org>
2016-05-17 18:11:43 +0100

    https by default

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-export PELICAN_SITEURL=http://www.hackzine.org
+export PELICAN_SITEURL=https://www.hackzine.org
 pelican -s pelicanconf.py -o ./output/ ./content/
 ghp-import -n -r production -b gh-pages ./output/
 git push -f production gh-pages
