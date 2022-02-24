7f2cdd1f8b4a1659f0b65e57e303ecd6e67eac34
Samuele Santi <samuele@samuelesanti.com>
2022-02-24 23:53:46 +0000

    map tool v2

--- src/pages/map-tool.js
+++ src/pages/map-tool.js
@@ -2,11 +2,10 @@ import * as React from "react";
 import { Link } from "gatsby";
 import { MapContainer, Marker, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Button, ButtonGroup, Input, Label } from "reactstrap";
+import { Button, ButtonGroup, Input, Label, InputGroup, InputGroupText } from "reactstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCrosshairs, faPencilAlt, faLocationArrow, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
-import Layout from "ui/layout";
 import "ui/map/default-icon";
 import GeodesicLine from "ui/map/geodesic-line";
 import GeodesicCircle from "ui/map/geodesic-circle";
@@ -14,6 +13,7 @@ import OsmTileLayer from "ui/map/osm-tile-layer";
 
 
 const DEFAULT_RESOLUTION = 180;
+const ENABLE_COORDINATE_INPUT = false;
 
 
 export default function MapToolPage() {
@@ -37,30 +37,45 @@ function MapTool() {
 
     // Points of interest to show on the map
     const [points, setPoints] = React.useState([
-        {location: [51.5, -6.5], radius: 200000, showRadius: true},
-        {location: [55.5, -9.2], radius: 200000, showRadius: true},
-        {location: [34.2, -80.3], radius: 200000, showRadius: true},
+        {location: [52.3, -6.6], radius: 200000, showRadius: true},
+        {location: [25.3,-80.7], radius: 200000, showRadius: true},
     ]);
 
     const locations = points.map(point => point.location);
 
-    const [center, setCenter] = React.useState([50, -6]);
-    const [isPickerActive, setPickerActive] = React.useState(false);
+    // Configuration for the picker tool
+    const [pickerTool, setPickerTool] = React.useState({
+        isActive: false,
+        callback: null,
+    });
 
-    const [circleRadius, setCircleRadius] = React.useState(200 * 1000);
-    const [showCircle, setShowCircle] = React.useState(false);
+    // Function to activate the picker tool
+    const activatePickerTool = (callback, options) => {
+        setPickerTool({
+            isActive: true,
+            callback,
+            label: "Pick a location on the map",
+            ...options,
+        });
+        selectTab("map");
+    };
 
-    const setCurrentLocation = ()=> {
-        navigator.geolocation.getCurrentPosition((position) => {
-            setCenter([position.coords.latitude, position.coords.longitude]);
+    const deactivatePickerTool = () => {
+        setPickerTool({
+            isActive: false,
         });
     };
 
     const onMapClick = event => {
         const {lat, lng} = event.latlng;
-        if (isPickerActive) {
-            setCenter([lat, lng]);
-            setPickerActive(false);
+        if (pickerTool.isActive) {
+            if (pickerTool.callback) {
+                pickerTool.callback([lat, lng]);
+            }
+            setPickerTool({
+                isActive: false,
+                callback: null,
+            });
         }
     };
 
@@ -92,50 +107,89 @@ function MapTool() {
             </div>
 
             {selectedTab === "map" &&
-             <MapContainer
-                 center={center} zoom={zoomLevel}
-                 style={{}}
-                 className="flex-grow-1"
-             >
+             <div className="flex-grow-1 d-flex flex-column">
 
-                 <MapEventHandler onClick={onMapClick} />
-                 <OsmTileLayer />
+                 {pickerTool.isActive &&
+                  <div className="bg-white text-black p-2 d-flex justify-content-between align-items-center">
+                      <div>{pickerTool.label}</div>
+                      <Button onClick={deactivatePickerTool}>
+                          Cancel
+                      </Button>
+                  </div>}
 
-                 {points.map((point, idx) =>
-                     <Marker key={idx} position={point.location} />)}
+                 <MapContainer
+                     center={points.length > 0 ? points[0].location : [0, 0]}
+                     zoom={zoomLevel}
+                     style={{}}
+                     className="flex-grow-1"
+                 >
 
-                 {points.map((point, idx) => {
-                     if (!point.showRadius) {
-                         return null;
+                     <MapEventHandler onClick={onMapClick} />
+                     <OsmTileLayer />
+
+                     {points.map((point, idx) =>
+                         <Marker key={idx} position={point.location} />)}
+
+                     {points.map((point, idx) => {
+                         if (!point.showRadius) {
+                             return null;
+                         }
+                         return (
+                             <GeodesicCircle
+                                 key={idx}
+                                 center={point.location}
+                                 radius={point.radius}
+                                 steps={DEFAULT_RESOLUTION}
+                             />);
+                     })}
+
+                     {locations.length > 1 &&
+                      <GeodesicLine
+                          positions={locations}
+                          steps={DEFAULT_RESOLUTION}
+                      />
                      }
-                     return (
-                         <GeodesicCircle
-                             key={idx}
-                             center={point.location}
-                             radius={point.radius}
-                             steps={DEFAULT_RESOLUTION}
-                         />);
-                 })}
-
-                 {locations.length > 1 &&
-                  <GeodesicLine
-                      positions={locations}
-                      steps={DEFAULT_RESOLUTION}
-                  />
-                 }
-             </MapContainer>}
+                 </MapContainer>
+             </div>}
 
             {selectedTab === "points" &&
              <PointsConfigurationPane
                  points={points}
                  setPoints={setPoints}
+                 activatePickerTool={activatePickerTool}
              />}
         </div>
     );
 }
 
 
-function PointsConfigurationPane({ points, setPoints }) {
+function PointsConfigurationPane({ points, setPoints, activatePickerTool }) {
+
+    const onPointChange = (idx, changes) => {
+        setPoints([
+            ...points.slice(0, idx),
+            changes,
+            ...points.slice(idx + 1),
+        ]);
+    };
+
+    const onPointDelete = (idx) => {
+        setPoints([
+            ...points.slice(0, idx),
+            ...points.slice(idx + 1),
+        ]);
+    };
+
+    const onPointAdd = (point) => {
+        setPoints([ ...points, point ]);
+    };
+
+    const onAddPickFromMap = () => {
+        activatePickerTool(location => {
+            onPointAdd({ location });
+        });
+    };
+
     return (
         <div>
             {points.map((point, idx) => (
@@ -143,44 +197,111 @@ function PointsConfigurationPane({ points, setPoints }) {
                     key={idx}
                     point={point}
                     idx={idx}
+                    activatePickerTool={activatePickerTool}
+                    onChange={onPointChange.bind(this, idx)}
+                    onDelete={onPointDelete.bind(this, idx)}
                 />
             ))}
             <div>
-                <Button title="Add from map" color="success" className="m-1">
+
+                <Button
+                    title="Add from map"
+                    color="success"
+                    className="m-1"
+                    onClick={onAddPickFromMap}
+                >
                     Add from map
                 </Button>
-                <Button title="Enter coordinates" color="success" className="m-1">
-                    Enter coordinates
-                </Button>
+
+                {ENABLE_COORDINATE_INPUT &&
+                 <Button title="Enter coordinates" color="success" className="m-1">
+                     Enter coordinates
+                 </Button>}
+
             </div>
         </div>
     );
 }
 
 
-function PointConfigurationRow({ point, idx, onChange }) {
+function PointConfigurationRow({ point, idx, onChange, onDelete, activatePickerTool }) {
+
+    const onPickFromMap = () => {
+        activatePickerTool(location => {
+            onChange({
+                ...point,
+                location,
+            });
+        });
+    };
+
+    const onPickCurrentLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            onChange({
+                ...point,
+                location: [latitude, longitude],
+            });
+        });
+
+    };
+
     return (
         <div className="d-flex flex-row align-items-center">
             <div className="p-1">{idx + 1}</div>
+
             <div className="p-1" style={{ minWidth: 300 }}>
                 <LocationDisplay location={point.location} />
             </div>
+
+            <div className="p-1">
+                <Label check>
+                    <Input
+                        type="checkbox"
+                        checked={point.showRadius}
+                        onChange={event => onChange({
+                            ...point,
+                            showRadius: event.target.checked,
+                        })}
+                    />
+                    {" "}Show radius
+                </Label>
+            </div>
+            <div className="p-1">
+                <InputGroup>
+                    <Input
+                        type="number"
+                        value={point.radius}
+                        onChange={event => onChange({
+                            ...point,
+                            radius: event.target.value,
+                        })}
+                        style={{maxWidth: 120, textAlign: 'right'}}
+                    />
+                    <InputGroupText className="text-white bg-dark">
+                        m
+                    </InputGroupText>
+                </InputGroup>
+            </div>
+
+            <div className="flex-grow-1" />
+
             <div className="p-1">
                 <ButtonGroup>
-                    <Button title="Enter coordinates">
-                        <FontAwesomeIcon icon={faPencilAlt} />
-                    </Button>
-                    <Button title="Pick from map">
+                    {ENABLE_COORDINATE_INPUT &&
+                     <Button title="Enter coordinates">
+                         <FontAwesomeIcon icon={faPencilAlt} />
+                     </Button>}
+                    <Button title="Pick from map" onClick={onPickFromMap}>
                         <FontAwesomeIcon icon={faCrosshairs} />
                     </Button>
-                    <Button title="Use current location">
+                    <Button title="Use current location" onClick={onPickCurrentLocation}>
                         <FontAwesomeIcon icon={faLocationArrow} />
                     </Button>
                 </ButtonGroup>
             </div>
-            <div className="flex-grow-1" />
             <div className="p-1">
-                <Button title="Use current location" color="danger">
+                <Button title="Use current location" color="danger" onClick={onDelete}>
                     <FontAwesomeIcon icon={faTrashAlt} />{" "}
                     Remove
                 </Button>
@@ -197,7 +318,9 @@ function LocationDisplay({ location }) {
 
     return (
         <div onClick={() => setNextFmtId()} className="mx-3">
-            <code>{formatter(location)}</code>
+            <code className="bg-dark text-white">
+                {formatter(location)}
+            </code>
         </div>
     );
 }
