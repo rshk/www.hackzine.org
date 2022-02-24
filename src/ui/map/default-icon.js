75d7035d4abb4936ba4894e5de863627010a8c54
Samuele Santi <samuele@samuelesanti.com>
2022-02-24 19:59:21 +0000

    Do not patch Leaflet unless it's loaded

--- src/ui/map/default-icon.js
+++ src/ui/map/default-icon.js
@@ -2,11 +2,17 @@ import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
-let DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-    iconSize: [25,41],
-    iconAnchor: [12,41],
-});
+if (L) {
 
-L.Marker.prototype.options.icon = DefaultIcon;
+    // L won't be set during SSR
+
+    let DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow,
+        iconSize: [25,41],
+        iconAnchor: [12,41],
+    });
+
+    L.Marker.prototype.options.icon = DefaultIcon;
+
+}
