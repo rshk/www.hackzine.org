a5e6032818fe7b0a0fff68c413dd86a9ae6925c0
Samuele Santi <samuele@samuelesanti.com>
2022-02-24 20:00:13 +0000

    Actual fix

--- src/ui/map/default-icon.js
+++ src/ui/map/default-icon.js
@@ -2,7 +2,7 @@ import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
-if (L) {
+if (L && L.icon) {
 
     // L won't be set during SSR
 
