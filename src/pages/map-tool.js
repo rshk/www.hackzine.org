c68c88ad0f48ea76850fadca69a7c0bc17f078f3
Samuele Santi <samuele@samuelesanti.com>
2022-05-08 15:24:37 +0100

    Redirect map-tool to ZenGIS

--- src/pages/map-tool.js
+++ src/pages/map-tool.js
@@ -1,7 +1,33 @@
 import * as React from "react";
-import MapToolApp from "components/map-tool-app";
+import Layout from "ui/layout";
+import { Button } from "reactstrap";
+
+
+const TARGET_URL = "https://zengis.xk0.org";
 
 
 export default function MapToolPage() {
-    return <MapToolApp />;
+    const [counter, setCounter] = React.useState(5);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            if (counter <= 0) {
+                window.location.href = TARGET_URL;
+            }
+            setCounter(x => Math.max(0, x - 1));
+        }, 1000);
+    }, [counter]);
+
+    return <Layout>
+        <div className="text-center">
+            <h1>Map tool is now ZenGIS</h1>
+            <div>It is now a stand-alone app, called ZenGIS.</div>
+            <div className="my-3">
+                <Button color="success" size="lg" tag="a" href={TARGET_URL}>
+                    Go to ZenGIS
+                </Button>
+            </div>
+            <div>You will be redirected in {counter} seconds...</div>
+        </div>
+    </Layout>;
 }
